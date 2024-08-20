3ba9f2631ef224a34a606ce5e2da89a83bd4d405
abeatrizdscoelho <anabeatrizdscoelho@gmail.com>
2024-08-20 17:42:15 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//document = ir no HTML
+//querySelector = busca o que eu preciso
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario(){
+formulario.style.left = "-50%"
+}
